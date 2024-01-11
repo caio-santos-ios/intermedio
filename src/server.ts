4d143abf6a54e5fb31fb1b7cd8582425b1411f50
caiosantos.dev@outlook.com <seu@email.com>
2024-01-11 02:09:52 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,58 @@ app.use(cors())
 const port = 3000;
 
 const api = axios.create({
-  baseURL: ""
+  baseURL: "https://controle-veiculo-c89a5c476b29.herokuapp.com"
 })
 
 // Middleware para tratar JSON
 app.use(express.json());
 
 // Rota de exemplo
-app.get('/', (req, res) => {
-  res.json({ message: 'Bem-vindo à minha API!' });
-});
+app.post('/owners', async (req, res) => {
+  try {
+    const owner = await api.post("/owners", req.body) 
+    res.json(req.body);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/owners', async (req, res) => {
+  try {
+    const owners = await api.get("/owners") 
+    res.json(owners.data);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/owners/:id', async (req, res) => {
+  try {
+    const owners = await api.get(`/owners/${req.params.id}`) 
+    res.json(owners.data);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.patch('/owners/:id', async (req, res) => {
+  try {
+    const owners = await api.patch(`/owners/${req.params.id}`, req.body) 
+    res.json(owners.data);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.delete('/owners/:id', async (req, res) => {
+  try {
+    const owners = await api.delete(`/owners/${req.params.id}`) 
+    res.json(owners.data);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 
 // Iniciar o servidor
 app.listen(port, () => {
